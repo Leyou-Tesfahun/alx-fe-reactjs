a97bbdc667635463fc02fe08509fdacd3602b295
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-07-25 07:54:50 -0700

    Task 2: Add Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,14 @@
 // src/App.jsx
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
