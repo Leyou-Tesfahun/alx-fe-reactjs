7090b36f7fc9b203815e8a3860731ae058dcd4a1
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-07-30 14:30:02 -0700

    123

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+        </div>
+    );
+}
+
+export default App;
