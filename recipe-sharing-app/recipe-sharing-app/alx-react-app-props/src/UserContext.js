2b40393ba280b7747379462dfd5b9d184679f315
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-07-30 15:17:39 -0700

    Delete recipe-sharing-app directory

--- recipe-sharing-app/recipe-sharing-app/alx-react-app-props/src/UserContext.js
+++
@@ -1,9 +0,0 @@
-// src/UserContext.js
-// src/UserContext.js
-// src/UserContext.js
-import React from "react";
-
-const UserContext = React.createContext();
-
-export default UserContext;
-8
--- recipe-sharing-app/src/UserContext.js
+++
@@ -1,8 +0,0 @@
-// src/UserContext.js
-// src/UserContext.js
-// src/UserContext.js
-// src/UserContext.js
-import { createContext } from 'react';
-
-const UserContext = createContext(null); // Initialize with null/default value
-export default UserContext;
