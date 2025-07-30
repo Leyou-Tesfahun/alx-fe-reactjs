34d8fc83f0cf757382b536f40bbb12ef113ef6af
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-07-30 15:47:51 -0700

    Completed Task 3: User Profile Card with Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,14 @@
 // src/App.jsx
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
+    <div className="App">
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
     </div>
   );
 }
