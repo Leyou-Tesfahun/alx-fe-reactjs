dee987636f460ee997313f814a5e72a1164f44a2
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-01 05:32:41 -0700

    Delete github-user-search/src directory

--- github-user-search/src/services/githubApi.js
+++
@@ -1,8 +0,0 @@
-const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_GITHUB_API_URL,
-  // headers: {
-  //   Authorization: import.meta.env.VITE_APP_GITHUB_TOKEN 
-  //     ? `token ${import.meta.env.VITE_APP_GITHUB_TOKEN}`
-  //     : undefined,
-  // }
-});
