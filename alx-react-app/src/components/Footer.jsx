450336bf0b37320f7aa2762b00c653c5788ce465
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-05 12:54:48 -0700

    Update Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,8 +1,9 @@
 function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#f2f2f2', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
-      <p>© 2023 City Lovers</p>
-    </footer>
-  );
+    return (
+        <footer>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
 }
+
 export default Footer;
