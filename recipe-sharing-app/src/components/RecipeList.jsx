b324e7b53e76d655cb2ded3ebc26a60dfdebb2f2
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-05 14:56:33 -0700

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,14 +1,14 @@
-import { useRecipeStore } from '../recipeStore';
+// src/components/RecipeList.jsx
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      <h2>Recipe List</h2>
-      {recipes.length === 0 && <p>No recipes yet.</p>}
       {recipes.map((recipe) => (
-        <div key={recipe.id} style={{ marginBottom: '1rem' }}>
+        <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
         </div>
