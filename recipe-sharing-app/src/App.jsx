34a2d31ec34d70ed93a59db28162379c2f24c748
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-05 15:16:49 -0700

    Update App.jsx
    Configure React Router in App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,19 +1,24 @@
-// src/App.jsx
 import React from 'react';
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+        <AddRecipeForm />
+      </div>
+    </Router>
   );
 };
 
 export default App;
-
