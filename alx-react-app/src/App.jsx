453f82522155e7ee3704c512ead48eb219353072
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-05 12:55:18 -0700

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,17 @@
 // src/App.jsx
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
     return (
-        <div>
+        <>
             <WelcomeMessage />
-        </div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
     );
 }
 
