f1c8cff8c9b497f1e077d2b04c7b07acc26dd714
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-05 15:40:37 -0700

    Update githubService.js
    Create a new function to build this query and fetch results

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
-// Function to fetch user data from GitHub API
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
-  } catch (error) {
-    throw error;
+export const fetchUsersAdvanced = async (username, location, minRepos) => {
+  let query = username || '';
+
+  if (location) {
+    query += ` location:${location}`;
   }
+  if (minRepos) {
+    query += ` repos:>=${minRepos}`;
+  }
+
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
+
+  const response = await axios.get(url);
+  return response.data;  // contains { total_count, items: [...] }
 };
