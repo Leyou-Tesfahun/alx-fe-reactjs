e8b8a90082b63f54110f8c4cb77f8034a3c662a0
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-19 01:24:24 -0700

    formikForm.js created

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,14 @@
 import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
     <div>
+      <h1>Controlled Form</h1>
       <RegistrationForm />
+
+      <h1>Formik Form</h1>
+      <FormikForm />
     </div>
   );
 }
