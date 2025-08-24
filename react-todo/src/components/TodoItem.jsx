5a0d21d0be60cab6a504ed7630032879163dbac5
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-24 08:38:46 -0700

    Update TodoItem.jsx

--- react-todo/src/components/TodoItem.jsx
+++ react-todo/src/components/TodoItem.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 
 const TodoItem = ({ todo, onToggle, onDelete }) => {
   return (
-    <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+    <li 
+      className={`todo-item ${todo.completed ? 'completed' : ''}`} 
+      role="listitem"
+      data-testid={`todo-item-${todo.id}`}
+    >
       <span
         onClick={() => onToggle(todo.id)}
         className={todo.completed ? 'completed-text' : ''}
+        role="checkbox"
+        aria-checked={todo.completed}
+        tabIndex={0}
+        onKeyPress={(e) => e.key === ' ' && onToggle(todo.id)}
+        data-testid="todo-toggle"
       >
         {todo.text}
       </span>
-      <button onClick={() => onDelete(todo.id)}>Delete</button>
+      <button 
+        onClick={() => onDelete(todo.id)}
+        aria-label={`Delete ${todo.text}`}
+        data-testid="delete-button"
+      >
+        Delete
+      </button>
     </li>
   );
 };
