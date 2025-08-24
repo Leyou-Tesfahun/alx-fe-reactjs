6fd7183fbcdb47cd3d5c59af7ad931f1c7fe4169
Leyou-Tesfahun <leyoutesfahun09@gmail.com>
2025-08-24 08:08:07 -0700

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -5,8 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+          <img src="/react.svg" className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>My Todo List</h1>
-      <TodoList />
+      <div className="card">
+        <TodoList />
+      </div>
+      <p className="read-the-docs">
+        Click on the React logo to learn more
+      </p>
     </div>
   );
 }
